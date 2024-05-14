0b5b1c82619185e6f74588d4add8518816df7239
eriusakiEl1 <dylanruelas13@gmail.com>
2024-05-14 07:43:12 -0600

    cambiando nombres de archivos
    Tal cual hermano

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 //routes
-app.use(require("./routes/index.js"));
+app.use(require("./routes/routes.js"));
 
 app.listen(4040);
 console.log("Server on port 4040");
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const router = Router();
 
-const { getUsers , createUser, getUserById, deleteUser, updateUser} = require('../controllers/index.controller');
+const { getUsers , createUser, getUserById, deleteUser, updateUser} = require('../controllers/bd.controller');
 
 router.get("/users", getUsers);
 router.post("/users", createUser);
