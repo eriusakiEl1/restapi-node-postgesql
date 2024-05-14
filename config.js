9b172da5ee0b5f2bf5bda2c75d851026ee1f38c6
eriusakiEl1 <dylanruelas13@gmail.com>
2024-05-14 11:19:39 -0600

    Configurando puerto
    Agregamos un puerto como si fuese una variable

--- config.js
+++ config.js
@@ -3,11 +3,13 @@ const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD || "admin";
 const DATABASE_NAME = process.env.DATABASE_NAME || "firstsapi";
 const DATABASE_PORT = process.env.DATABASE_PORT || "5454";
 const DATABASE_USER = process.env.DATABASE_USER || "admin";
+const PORT = process.env.PORT || "4040";
 
 module.exports = {
     DATABASE_HOST,
     DATABASE_NAME,
     DATABASE_PASSWORD,
     DATABASE_PORT,
-    DATABASE_USER
+    DATABASE_USER,
+    PORT
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const {PORT} = require('../../config')
 
 //middlewares
 app.use(express.json());
@@ -8,5 +9,5 @@ app.use(express.urlencoded({ extended: false }));
 //routes
 app.use(require("./routes/routes.js"));
 
-app.listen(4040);
+app.listen(PORT);
 console.log("Server on port 4040");
